264ae3e14076f4eb69d8f60447847177744e3fe6
MoaazK <moaazk141@gmail.com>
2025-11-09 23:36:39 +0300

    updated name

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://moaazk.github.io/koccricket',
-	base: '/koccricket',
+	base: '/koc-cricket',
 });
