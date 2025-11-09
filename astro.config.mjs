7fd022d4db05240ffa06ce1ca965e3070f295c2f
MoaazK <moaazk141@gmail.com>
2025-11-09 23:48:39 +0300

    added workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://moaazk.github.io/koccricket',
+	site: 'https://moaazk.github.io/koc-cricket',
 	base: '/koc-cricket',
 });
